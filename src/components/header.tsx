b440f053fdec5332611cd3900c7f20baff1ed873
uzweb-code <ismatovsanjarbek@yandex.ru>
2022-05-18 21:19:04 +0500

    updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,20 @@
+import Logo from "../images/app-store.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 export default function Header() {
   return (
     <>
       <header>
         <div className="header">
           <div className="logo">
-            <img />
-            <p>Ready</p>
+            <img src={Logo} alt="logo" />
+            <h1>App Store</h1>
+          </div>
+          <div className="input">
+            <div className="icon">
+              <FontAwesomeIcon className="search-icon" icon={faSearch} />
+            </div>
+            <input type="text" />
           </div>
         </div>
       </header>
